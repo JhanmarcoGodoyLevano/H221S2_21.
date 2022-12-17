2d2bbbed4ffa6b90817b83cecdf42eecd2e22e90
Jhanmarco Godoy Levano <111781171+JhanmarcoGodoyLevano@users.noreply.github.com>
2022-12-17 12:52:57 -0500

    db2

--- conexion.js
+++ conexion.js
@@ -1,18 +1,49 @@
-var express = require("express");
-var mysql = require("mysql");
+var express = require('express');
+  
+
 var app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + "/login.html");
+});
+
+const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+//Recursos
+app.use(express.static(__dirname+'/'));
+
+
+
+
+
+
+var mysql = require("mysql");
+
 var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-var conexion = mysql.createConnection({
-    host: "52.203.124.40",
-    user: "jhanmarco",
-    password: "1234",
-    database: "dbRestaurante",
-  });
 
-  conexion.connect(function (error) {
+var conexion = mysql.createConnection({
+  host: "52.203.124.40",
+  user: "jhanmarco",
+  password: "1234",
+  database: "dbRestaurante",
+});
+
+conexion.connect(function (error) {
     if (error) {
       throw error;
     } else {
@@ -20,13 +51,12 @@ var conexion = mysql.createConnection({
     }
   });
 
-  const puerto = process.env.PUERTO || 3000;
-
+  
 
 app.post("/api/pedido", (req, res) => {
 	let data = {
     	userped: req.body.USERPED,
-        emausped: req.body.EMAUSPED,
+    	emausped: req.body.EMAUSPED,
     	celusped: req.body.CELUSPED,
     	foodped: req.body.FOODPED,
     	msgped: req.body.MSGPED
@@ -41,19 +71,9 @@ app.post("/api/pedido", (req, res) => {
   	}
 	});
   });
-  var express = require('express');
-  var app = express();
-  
-  //Recursos
-  app.use(express.static(__dirname+'/'));
-  
-  app.listen(3000);
-  console.log('server on port 3000');
-  
-  app.get('/', function (req, res) {
-    res.sendFile(dirname + "/index.html");
-});
 
-app.get('/login', function (req, res) {
-    res.sendFile(dirname + "/login.html");
-});
+
+
+
+
+
--- registro.js
+++ registro.js
@@ -15,40 +15,40 @@ btnCrear.addEventListener('click', () => {
 
 formArticulo.addEventListener('submit',
     (e) => {
-     e.preventDefault();
-     if (opcion == 'crear') {
-         if (USERPED.value == "" || EMAUSPED.value == "" || CELUSPED.value == "" || FOODPED.value == "" || MSGPED.value == "") {
-             alert("Asegúrese de que todos los campos estén completos");
-             return false;
-         } else {
-             console.log("Todos los campos están completos");
-             fetch(
-                 url,
-                 {
-                     method: 'POST',
-                     headers: {
-                         'content-Type':'application/json'
-                     },
-                     body: JSON.stringify(
-                         {
-                             USERPED: USERPED.value,
-                             EMAUSPED: EMAUSPED.value,
-                             CELUSPED: CELUSPED.value,
-                             FOODPED: FOODPED.value,
-                             MSGPED: MSGPED.value
-                         }
-                     )
-                 }
-             )
-             .then(
-                 response => response.json()
-             )
-             .then(
-                 response => location.reload()
-             );
-         }
-     } else if(opcion == 'editar'){
-         console.log("Activado el ");
-     }
+   	 e.preventDefault();
+   	 if (opcion == 'crear') {
+   		 if (USERPED.value == "" || EMAUSPED.value == "" || CELUSPED.value == "" || FOODPED.value == "" || MSGPED.value == "") {
+       		 alert("Asegúrese de que todos los campos estén completos");
+       		 return false;
+   		 } else {
+       		 console.log("Todos los campos están completos");
+       		 fetch(
+           		 url,
+           		 {
+               		 method: 'POST',
+               		 headers: {
+                   		 'content-Type':'application/json'
+               		 },
+               		 body: JSON.stringify(
+                   		 {
+                       		 USERPED: USERPED.value,
+                       		 EMAUSPED: EMAUSPED.value,
+                       		 CELUSPED: CELUSPED.value,
+                       		 FOODPED: FOODPED.value,
+                       		 MSGPED: MSGPED.value
+                   		 }
+               		 )
+           		 }
+       		 )
+       		 .then(
+           		 response => response.json()
+       		 )
+       		 .then(
+           		 response => location.reload()
+       		 );
+   		 }
+   	 } else if(opcion == 'editar'){
+   		 console.log("Activado el ");
+   	 }
     }
 );
