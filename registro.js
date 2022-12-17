1e42a7ab3f81ef9dfcfadd919e936bc23fe9f94a
Jhanmarco Godoy Levano <111781171+JhanmarcoGodoyLevano@users.noreply.github.com>
2022-12-17 13:04:18 -0500

    db3

--- registro.js
+++ registro.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/api/pedido/";
+const url = "http://52.203.124.40:3000/api/pedido/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
